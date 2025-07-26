4f92f5ce26fbd0533c9f2a40888a5339d82d8e9d
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-07-26 14:55:14 +0500

    replace function base views with  generic (class base) views

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
-from .views import product_list, product_detail, order_list, product_info
+from .views import ProductListApiView, ProductDetailApiView, OrderListApiView, product_info
 from django.urls import path
 
 app_name='api'
 
 urlpatterns = [
-    path('products/', product_list, name='products'),
-    path('products/<int:pk>', product_detail, name='product_details'),
-    path('orders/', order_list, name='orders'),
+    path('products/', ProductListApiView.as_view(), name='products'),
+    path('products/<int:product_id>', ProductDetailApiView.as_view(), name='product_details'),
+    path('orders/', OrderListApiView.as_view(), name='orders'),
     path('products/info', product_info, name='product_info'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import get_object_or_404
 from .models import Product, Order, OrderItem
 from .serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.db.models import Max
+from rest_framework import generics
 
+
+class ProductListApiView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailApiView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+
+class OrderListApiView(generics.ListAPIView):
+    queryset = Order.objects.select_related('user').prefetch_related('items', 'items__product').all()
+    serializer_class = OrderSerializer
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),        
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
+    return Response(data=serializer.data)
+
+
+
+'''
 @api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
@@ -25,13 +55,4 @@ def order_list(request):
     serializer = OrderSerializer(orders, many=True)
     return Response(data=serializer.data)
 
-
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products': products,
-        'count': len(products),        
-        'max_price': products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(data=serializer.data)
+'''
